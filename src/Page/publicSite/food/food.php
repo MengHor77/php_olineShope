1ff0c6d2a320ac62df0c002249940a01637c119b
meng hor <piseth125125@gmail.com>
2024-09-20 04:19:31 +0700

    fix: path include

--- src/Page/publicSite/food/food.php
+++ src/Page/publicSite/food/food.php
@@ -1,15 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>food</title>
-    <link href="../../../../dist/styles.css" rel="stylesheet">
+    <!-- <link href="../../../../dist/styles.css" rel="stylesheet">  -->
+    <link href="/php/src/dist/styles.css" rel="stylesheet">
 </head>
+
 <body>
-    <div>
-         <?php include '../Layout/Header/headerPubliceSite.php';?>
+<div>
+        <!-- Include the header -->
+        <?php include __DIR__ . '/../Layout/Header/headerPubliceSite.php'; ?>
+
+        <!-- Include the food index.php -->
+        <?php include __DIR__ . '/index.php'; ?>
     </div>
-    
+
 </body>
+
 </html>
