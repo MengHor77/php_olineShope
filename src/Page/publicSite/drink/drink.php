2eec7bb510d0c85c8ff7a535d5889c810ea457af
meng hor <piseth125125@gmail.com>
2024-09-20 04:19:05 +0700

    fix: path include

--- src/Page/publicSite/drink/drink.php
+++ src/Page/publicSite/drink/drink.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>drink</title>
-    <link href="../../../../dist/styles.css" rel="stylesheet">
+    <!-- <link href="../../../../dist/styles.css" rel="stylesheet"> -->
+    <link href="/php/src/dist/styles.css" rel="stylesheet">
 </head>
 <body>
     <div>
-    <?php include '../Layout/Header/headerPubliceSite.php';?>
+    <?php include __DIR__ . '/../Layout/Header/headerPubliceSite.php'; ?>
+    <?php include __DIR__ . '/index.php'; ?>
     </div>
     
 </body>
