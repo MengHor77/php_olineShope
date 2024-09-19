5783c7a074682022e7dbb9edaf7f1d61a68fe099
meng hor <piseth125125@gmail.com>
2024-09-20 04:19:59 +0700

    fix: path include

--- src/Page/publicSite/homeAndLifeStyle/homeAndLifeStyle.php
+++ src/Page/publicSite/homeAndLifeStyle/homeAndLifeStyle.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>home and life style</title>
-    <link href="../../../../dist/styles.css" rel="stylesheet">
+    <!-- <link href="../../../../dist/styles.css" rel="stylesheet"> -->
+    <link href="/php/src/dist/styles.css" rel="stylesheet">
 </head>
 <body>
-    <div>
-         <?php include '../Layout/Header/headerPubliceSite.php';?>
+<div>
+        <!-- Include the header -->
+        <?php include __DIR__ . '/../Layout/Header/headerPubliceSite.php'; ?>
+
+        <!-- Include the food index.php -->
+        <?php include __DIR__ . '/index.php'; ?>
     </div>
     
 </body>
