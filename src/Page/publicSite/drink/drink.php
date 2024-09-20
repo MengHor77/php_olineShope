e7d59cc7d17a06eedc1dd6a3ee5c9c3d61aca134
meng hor <piseth125125@gmail.com>
2024-09-21 03:04:11 +0700

    fix: include footer.php

--- src/Page/publicSite/drink/drink.php
+++ src/Page/publicSite/drink/drink.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>drink</title>
-    <!-- <link href="../../../../dist/styles.css" rel="stylesheet"> -->
+    <title>Drink</title>
     <link href="/php/src/dist/styles.css" rel="stylesheet">
 </head>
+
 <body>
-    <div>
-    <?php include __DIR__ . '/../Layout/Header/headerPubliceSite.php'; ?>
-    <?php include __DIR__ . '/index.php'; ?>
-    </div>
-    
+
+    <header>
+        <?php include __DIR__ . '/../Layout/Header/headerPubliceSite.php'; ?>
+    </header>
+
+    <main>
+        <?php include __DIR__ . '/index.php'; ?>
+    </main>
+
+    <footer>
+        <?php include __DIR__ . '/../Layout/Footer/footer.php'; ?>
+    </footer>
+
 </body>
+
 </html>
