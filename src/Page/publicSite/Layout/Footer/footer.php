ed3031aab51202d477d400237a9dc1946e27c3a6
meng hor <piseth125125@gmail.com>
2024-09-20 20:15:17 +0700

    Task: add footer.php

---
+++ src/Page/publicSite/Layout/Footer/footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer </title>
+    <link href="/php/src/dist/styles.css" rel="stylesheet">
+
+</head>
+
+<footer class="bg-gray-800 text-white py-8 ">
+    <div class="container grid grid-cols-2 md:grid-cols-4 gap-8 items-center justify-center">
+        <!-- About Section -->
+        <div class=" flex justify-center">
+            <div class="flex flex-col  justify-start">
+                <h5 class="text-lg font-bold mb-4 ">About</h5>
+                <p class="text-sm hover:underline">Our Story</p>
+                <p class="text-sm hover:underline">Meet the Team</p>
+                <p class="text-sm hover:underline">Careers</p>
+                <p class="text-sm hover:underline">Latest Blog Posts</p>
+
+            </div>
+        </div>
+        <!-- Help Section -->
+        <div class=" flex justify-center">
+            <div class="flex flex-col  justify-start">
+
+                <h5 class="text-lg font-bold mb-4 ">Help</h5>
+                <p class="text-sm hover:underline">FAQs</p>
+                <p class="text-sm hover:underline">Contact Support</p>
+                <p class="text-sm hover:underline">Customer Service</p>
+                <p class="text-sm hover:underline">Return Policy</p>
+
+
+            </div>
+        </div>
+        <!-- Account Section -->
+        <div class=" flex justify-center">
+            <div class="flex flex-col justify-start">
+                <h5 class="text-lg font-bold mb-4">Account</h5>
+
+                <p class="text-sm hover:underline">Login</p>
+                <p class="text-sm hover:underline">Register</p>
+                <p class="text-sm hover:underline">My Profile</p>
+                <p class="text-sm hover:underline">Order History</p>
+            </div>
+        </div>
+        <!-- Brands Section -->
+        <div class=" flex justify-center">
+            <div class="flex flex-col justify-start">
+                <h5 class="text-lg font-bold mb-4 ">Brands</h5>
+
+                <p class="text-sm hover:underline">Brand A</p>
+                <p class="text-sm hover:underline">Brand B</p>
+                <p class="text-sm hover:underline">Brand C</p>
+                <p class="text-sm hover:underline">Brand D</p>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="mt-8 text-center text-sm">
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+    </div>
+</footer>
+</html>
