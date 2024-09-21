35a61289ec6d14b69c7cf0a0af4ec3b54c12d21e
meng hor <piseth125125@gmail.com>
2024-09-21 14:10:49 +0700

    fix: remove comment

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,36 +1,34 @@
-
 <?php
 
 class HomeController
 {
-    // Method to render the home page
+    
     public function showHome()
     {
-        include __DIR__ . '/../../src/Page/publicSite/home/home.php'; // Adjust path as necessary
+        include __DIR__ . '/../../src/Page/publicSite/home/home.php'; 
     }
 
-    // Method to render the food page
     public function showFood()
     {
-        include __DIR__ . '/../../src/Page/publicSite/food/food.php'; // Adjust path as necessary
+        include __DIR__ . '/../../src/Page/publicSite/food/food.php'; 
     }
-
-    // Method to render the drink page
+  
     public function showDrink()
     {
-        include __DIR__ . '/../../src/Page/publicSite/drink/drink.php'; // Adjust path as necessary
+        include __DIR__ .'/../../src/Page/publicSite/drink/drink.php'; 
     }
-
-    // Method to render the health and beauty page
+   
     public function showHealthBeauty()
     {
-        include __DIR__ . '/../../src/Page/publicSite/healthbeauty/healthbeauty.php'; // Adjust path as necessary
+        include __DIR__ . '/../../src/Page/publicSite/healthAndBeauty/healthAndBeauty.php'; 
     }
-
-    // Method to render the home and lifestyle page
+    
     public function showHomeLifestyle()
     {
-        include __DIR__ . '/../../src/Page/publicSite/homelifestyle/homelifestyle.php'; // Adjust path as necessary
+        include __DIR__ . '/../../src/Page/publicSite/homeAndLifestyle/homeAndLifestyle.php'; 
+    }
+    public function showNew()
+    {
+        include __DIR__ . '/../../src/Page/publicSite/new/new.php';
     }
 }
-
