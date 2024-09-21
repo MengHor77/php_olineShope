4d062efbf90aea5808dccf7ef1946304295ddd42
meng hor <piseth125125@gmail.com>
2024-09-21 15:46:42 +0700

    fix: remove other controller

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -8,27 +8,4 @@ class HomeController
         include __DIR__ . '/../../src/Page/publicSite/home/home.php'; 
     }
 
-    public function showFood()
-    {
-        include __DIR__ . '/../../src/Page/publicSite/food/food.php'; 
-    }
-  
-    public function showDrink()
-    {
-        include __DIR__ .'/../../src/Page/publicSite/drink/drink.php'; 
-    }
-   
-    public function showHealthBeauty()
-    {
-        include __DIR__ . '/../../src/Page/publicSite/healthAndBeauty/healthAndBeauty.php'; 
-    }
-    
-    public function showHomeLifestyle()
-    {
-        include __DIR__ . '/../../src/Page/publicSite/homeAndLifestyle/homeAndLifestyle.php'; 
-    }
-    public function showNew()
-    {
-        include __DIR__ . '/../../src/Page/publicSite/new/new.php';
-    }
 }
