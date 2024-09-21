766d393dbf7121c76156f1d5769d7d3e46079724
meng hor <piseth125125@gmail.com>
2024-09-21 15:46:07 +0700

    Task: add HealthBeautyController.php

---
+++ app/controllers/HealthBeautyController.php
@@ -0,0 +1,9 @@
+<?php
+
+class HealthBeautyController
+{
+    public function showHealthBeauty()
+    {
+        include __DIR__ . '/../../src/Page/publicSite/healthAndBeauty/healthAndBeauty.php';
+    }
+}
