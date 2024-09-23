2e66aac3d87d2f9ac3d08fb09cbd6a752e6eeff1
meng hor <piseth125125@gmail.com>
2024-09-23 21:09:13 +0700

    fix: css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': 'rgb(156, 197, 75)', // Main custom primary color
-        'primary-30': 'rgb(193, 219, 155)', // Example lighter shade (30%)
-        'primary-50': 'rgb(174, 208, 115)', // Example lighter shade (50%)
+        'primary': 'rgb(156, 197, 75)',  // Main custom primary color
+        'primary-30': 'rgb(193, 219, 155)', // Lighter shade (30%)
+        'primary-50': 'rgb(174, 208, 115)', // Lighter shade (50%)
         'primary-70': 'rgb(156, 197, 75)',  // Main primary color (70%)
         'primary-90': 'rgb(137, 175, 56)',  // Darker shade (90%)
         'primary-100': 'rgb(117, 154, 36)', // Darkest shade (100%)
