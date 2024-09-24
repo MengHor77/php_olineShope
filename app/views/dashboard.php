3ac528f489988e50834c72be87412486bf35f8af
meng hor <piseth125125@gmail.com>
2024-09-24 23:37:17 +0700

    fix: logout

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -1,6 +1,4 @@
 <?php
-
-
 if (!isset($_SESSION['user'])) {
     header('Location: /php/src/login');
     exit;
@@ -11,17 +9,22 @@ $controller->index();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
+    <link href="http://localhost/php/dist/styles.css" rel="stylesheet"> <!-- Corrected path -->
+    
 </head>
 <body>
-    <div>
-    <h1>Welcome, <?php echo htmlspecialchars($_SESSION['user']['username']); ?>!</h1>
-    <a href="/php/src/logout">Logout</a>
     
-    </div>
-   
+<?php
+include_once 'comfirmLogout.php';
+?>
 </body>
+
+
 </html>
