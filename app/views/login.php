449f2b7bd2521d7b42d6aff352624175208c1bc2
meng hor <piseth125125@gmail.com>
2024-09-24 23:37:31 +0700

    fix: css

--- app/views/login.php
+++ app/views/login.php
@@ -5,27 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="/php/src/dist/styles.css" rel="stylesheet">
+    <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
+    <link href="http://localhost/php/dist/styles.css" rel="stylesheet"> <!-- Corrected path -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
+    
 </head>
 
 <body>
  <div class="flex justify-center items-center min-h-screen ">
-        <div class="login-container  flex flex-col  justify-center items-center mt-10 bg-gray-200 ">
+        <div class="login-container  flex flex-col  justify-center items-center mt-10 bg-gray-200 rounded-md pb-10 ">
             <form method="POST" action="/php/src/login" class="p-10 w-96 ">
                 <h2 class ="font-bold text-primary text-center text-2xl">Login form</h2>
                 <?php if (isset($error)): ?>
                 <p class="error"><?php echo $error; ?></p>
                 <?php endif; ?>
                 <div class="flex flex-col gap-1 pt-4">
-                    <label for="">Username</label>
-                    <input type="text" name="username" placeholder="Username" required>
+                    <label for="" class="font-bold">Username</label>
+                    <input type="text" name="username" placeholder="Username" required class=" h-10 rounded-md">
                 </div>
                 <div class="flex flex-col gap-1 pt-4">
-                    <label for="">Password</label>
-                    <input type="password" name="password" placeholder="Password" required>
+                    <label for="" class="font-bold">Password</label>
+                    <input type="password" name="password" placeholder="Password" required  class=" h-10 rounded-md">
                 </div>
                 <div class="mt-10">
-                    <button type="submit" class="w-32 h-10 bg-primary-100 rounded-md">Login</button>
+                    <button type="submit" class="w-32 h-10 bg-primary rounded-md hover:bg-primary-100 font-bold">Login</button>
                 </div>
             </form>
             <p class="font-bold">Already have an account?
